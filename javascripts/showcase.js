b6309aae7209aba4c0569ffef54cc561e62c6560
Aupajo <pete@metanation.com>
2008-11-21 11:31:20 +1300

    Updated showcase javascriptings.

--- javascripts/showcase.js
+++ javascripts/showcase.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-  var selected = $("#showcase .first");
+  var selected;
   
-  $("#showcase .nav a").click( function() {
-    if($(this) != selected) {
-      $('#showcase .nav a.selected').removeClass('selected');
-      selected.hide();
+  $("#showcase .nav a").each(function() {
+    if($(this).hasClass('selected')) {
       selected = $($(this).attr('href'));
       selected.show();
-      $(this).addClass('selected');
-      return false; // Stops href from jumping to the anchor
+    } else {
+      $($(this).attr('href')).hide();
     }
-  })
+    
+    $(this).click( function() {
+      if($(this) != selected) {
+        $('#showcase .nav a.selected').removeClass('selected');
+        selected.hide();
+        selected = $($(this).attr('href'));
+        selected.show();
+        $(this).addClass('selected');
+        return false; // Stops href from jumping to the anchor
+      }
+    });
+  
+  });
+  
 });
